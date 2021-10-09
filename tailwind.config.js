f31236fbba5a55a15177b4f1604837a3c726aba4
Genady Shmunik <genady.shmunik@ge.com>
2021-10-10 01:17:39 +0300

    + Shared StoryBook for all Angular UI components
    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,38 @@
-const { guessProductionMode } = require("@ngneat/tailwind");
+const { guessProductionMode } = require('@ngneat/tailwind');
 
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './apps/**/*.{html,ts,css,scss,sass,less,styl}',
-        './libs/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
-    },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    content: [
+      './apps/**/*.{html,ts,css,scss,sass,less,styl}',
+      './libs/**/*.{html,ts,css,scss,sass,less,styl}'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        header: ['Montserrat Alternates']
+      },
+      backgroundColor: {
+        'primary': '#f3f3f3',
+        'secondary': '#fff'
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/assets/img/hero.png')",
+        'hero-mobile-pattern': "url('/assets/img/hero-mobile.png')",
+      },
+      height: {
+        hero: '470px',
+        'mobile-hero': '300px'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')]
 };
